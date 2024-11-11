a5557332affe6a51c194d4a34f1e196b3387972e
NataliaSerantesCortez <nataliaayelenser.2023.alumn@cescristorey.com>
2024-11-11 21:55:17 +0100

    v2 vuelos

--- Vuelos/Vuelos/Form1.cs
+++ Vuelos/Vuelos/Form1.cs
@@ -28,31 +28,39 @@ namespace Vuelos
         {
             String origen = comboBox1.Text;
             String destino = comboBox2.Text;
-
-            if (origen.Equals(destino))
-            {
-                MessageBox.Show("El destino no puede ser igual al origen.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-
             DateTime fechaIdaSeleccionada = dateTimePicker2.Value;
             DateTime fechaVueltaSeleccionada = dateTimePicker1.Value;
 
-            if (checkBox1.Checked)
-            {
-                if (fechaIdaSeleccionada > fechaVueltaSeleccionada)
-                {
-                    MessageBox.Show("La fecha de vuelta no puede ser anterior a la fecha de ida.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+           
 
+            if (origen.Equals("") || destino.Equals(""))
+            {
+                MessageBox.Show("No puede estar vacío el origen o destino.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            } 
+            else if (origen.Equals(destino))
+            {
+                MessageBox.Show("El destino no puede ser igual al origen.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (checkBox1.Checked)
+                {
+                    if (fechaIdaSeleccionada > fechaVueltaSeleccionada)
+                    {
+                        MessageBox.Show("La fecha de vuelta no puede ser anterior a la fecha de ida.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    }
+                    else /*if (!origen.Equals(destino))*/
+                    {
+                        MessageBox.Show("Estamos buscando su vuelo...", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    }
                 }
-                else if (!origen.Equals(destino) && !origen.Equals("") && destino != null)
+                else /*if (!origen.Equals(destino))*/
                 {
                     MessageBox.Show("Estamos buscando su vuelo...", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
                 }
-            } else if (!origen.Equals(destino) && !origen.Equals("") && destino != null)
-            {
-                MessageBox.Show("Estamos buscando su vuelo...", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
             }
 
 
