75b60695fe5946598ccb8cd19e334b4165ee8d2a
NataliaSerantesCortez <nataliaayelenser.2023.alumn@cescristorey.com>
2025-02-04 18:06:21 +0100

    rotacion y puntos

--- flappybird/scripts/marciano.gd
+++ flappybird/scripts/marciano.gd
@@ -3,11 +3,13 @@ extends CharacterBody2D
 const GRAVITY = 980.0
 const JUMP_VELOCITY = -300.0
 var juego_iniciado = false
+var max_rotation = 15.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if juego_iniciado:
 		velocity.y += GRAVITY*delta
+		rotation_degrees = clamp(velocity.y * 0.1, -max_rotation, max_rotation)
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept"):
